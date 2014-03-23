c05e863ae63d1347d4636700097bab1018337396
jack <jack@172.26.70.245>
2014-03-23 15:21:32 +0800

    insert Camera controll class and it's interface file

---
+++ src/smartcar/Sensor/CameraHW.java
@@ -0,0 +1,11 @@
+package smartcar.Sensor;
+
+/**
+ * The CameraHW class is to manage the samera hardware in the system,
+ * and provice related functions for other classes to call,
+ * such as camera controlling and providing image
+ * @author jack
+ */
+public class CameraHW implements CameraHWIf{
+    
+}
