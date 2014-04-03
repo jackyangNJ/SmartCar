c50f4f2873d41ab39fbf48926cd70218c7d3139e
Administrator <Administrator@172.26.67.135>
2014-04-03 23:17:12 +0800

    map接口函数需要返回的类

---
+++ src/smartcar/SmartMapInterface.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package smartcar;
+
+/**
+ *
+ * @author Administrator
+ */
+public interface SmartMapInterface {
+    public void getBarrierInformation();//地图整体障碍物信息
+    public void getQRCodeInformation();//地图整体二维码位置信息
+    public void getBarrierInformation(float x,float y);//地图中某点周围障碍物信息
+    public void getQRCodeInformation(float x,float y);//地图中某点周围二维码位置信息
+    public SmartMapData getPath(float start_x,float start_y,float end_x,float end_y);
+}
