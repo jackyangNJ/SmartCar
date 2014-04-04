fb03662ee957918955d065e57b8e27b82247f566
JackYang <jackyang7474@gmail.com>
2014-04-04 20:35:18 +0800

    完成了Camera的实现和测试代码

--- src/smartcar/Sensor/CameraHW.java
+++ src/smartcar/Sensor/CameraHW.java
@@ -1,5 +1,6 @@
 package smartcar.Sensor;
 
+import com.googlecode.javacv.cpp.opencv_core;
 import com.googlecode.javacv.cpp.opencv_highgui;
 
 /**
@@ -13,7 +14,19 @@ public class CameraHW implements CameraHWIf {
 
     private static opencv_highgui.CvCapture cvCapture;
 
-    static {
-        opencv_highgui.CvCapture cvCapture = opencv_highgui.cvCreateCameraCapture(0);
+    public static void startCamera() {
+        cvCapture = opencv_highgui.cvCreateCameraCapture(0);
+        if (cvCapture == null) {
+            System.err.println("cannot get camera");
+            System.exit(-1);
+        }
+    }
+
+    public static void stopCamera() {
+        opencv_highgui.cvReleaseCapture(cvCapture);
+    }
+
+    public static opencv_core.IplImage getImage() {
+        return opencv_highgui.cvQueryFrame(cvCapture);
     }
 }
--- src/test/CameraTest.java
+++ src/test/CameraTest.java
@@ -1,11 +1,29 @@
 package test;
 
+import com.googlecode.javacv.cpp.opencv_core;
+import com.googlecode.javacv.cpp.opencv_highgui;
+import smartcar.Sensor.CameraHW;
+
 /**
  * test if CameraHW works
+ *
  * @author jack
  */
 public class CameraTest {
-    public static void main(String []args){
+
+    public static void main(String[] args)  {
+        CameraHW.startCamera();
+        opencv_highgui.cvNamedWindow("test");
+
+        while (true) {
+            opencv_core.IplImage frame = CameraHW.getImage();
+            opencv_highgui.cvShowImage("test", frame);
+            if (opencv_highgui.cvWaitKey(30) == 27) {
+                break;
+            }
+        }
         
+        opencv_highgui.cvDestroyWindow("test");
+        CameraHW.stopCamera();
     }
 }
