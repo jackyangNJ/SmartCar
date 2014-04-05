9d8d9d59a69a6536cf8eece5520ac985696b9d9d
Administrator <Administrator@114.212.130.219>
2014-04-05 20:13:21 +0800

    map接口更改，以及维护障碍物和二维码的类

---
+++ src/smartcar/SmartMapBarrier.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package smartcar;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Administrator
+ */
+public class SmartMapBarrier {
+    int num;//障碍物个数
+    ArrayList<Barrier> barriers = new ArrayList<Barrier>();
+    void setBarrier(float x,float y,float length,float width,String s) {
+        Barrier b = new Barrier();
+        b.x = x;
+        b.y = y;
+        b.length = length;
+        b.width = width;
+        b.description = s;
+        barriers.add(b);
+        num ++;
+    }
+    void printBarriers() {
+        System.out.println("The number of barriers is " + num);
+        for(int i = 0;i < num;i++) {
+            System.out.println(i + ": " + barriers.get(i).x + "," + barriers.get(i).y + "," + barriers.get(i).length
+                    + "," + barriers.get(i).width + "," + barriers.get(i).description);
+        }
+    }
+    void printBarriers(float x,float y) {
+        for(int i = 0;i < num;i++) {
+            float distance = (barriers.get(i).y - y) * (barriers.get(i).y - y) 
+                    + (barriers.get(i).x - x) * (barriers.get(i).x - x);
+            if(distance < 4)
+            System.out.println(barriers.get(i).x + "," + barriers.get(i).y + "," + barriers.get(i).length
+                    + "," + barriers.get(i).width + "," + barriers.get(i).description);
+        }
+    }
+    class Barrier {
+        float x;//中心点横坐标
+        float y;//中心点纵坐标
+        float length;
+        float width;
+        String description = new String();
+    }
+}
--- src/smartcar/SmartMapInterface.java
+++ src/smartcar/SmartMapInterface.java
@@ -11,9 +11,9 @@ package smartcar;
  * @author Administrator
  */
 public interface SmartMapInterface {
-    public void getBarrierInformation();//地图整体障碍物信息
-    public void getQRCodeInformation();//地图整体二维码位置信息
-    public void getBarrierInformation(float x,float y);//地图中某点周围障碍物信息
-    public void getQRCodeInformation(float x,float y);//地图中某点周围二维码位置信息
+    public SmartMapBarrier getBarrierInformation();//地图整体障碍物信息
+    public SmartMapQRCode getQRCodeInformation();//地图整体二维码位置信息
+    public SmartMapBarrier getBarrierInformation(float x,float y);//地图中某点周围障碍物信息
+    public SmartMapQRCode getQRCodeInformation(float x,float y);//地图中某点周围二维码位置信息
     public SmartMapData getPath(float start_x,float start_y,float end_x,float end_y);
 }
