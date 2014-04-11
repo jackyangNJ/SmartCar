1d52f0990bb6f14193b61b152342ac1ead6fc0d9
jack yang <467287824@qq.com>
2014-04-11 20:17:42 +0800

    修改了Interactor 和 InterractorIf

--- src/smartcar/Interactor/InteractorIf.java
+++ src/smartcar/Interactor/InteractorIf.java
@@ -1,6 +1,16 @@
 package smartcar.Interactor;
 
+import smartcar.SmartMap;
+
 
 public interface InteractorIf {
-    
+    public void setOperation(int op);
+    //set the location of the car
+    public void setCarLocation(double x,double y);
+    //set the location of the book
+    public void setBookLocation(double x,double y);
+    public SmartMap getMap();
+    public CarLocation getCarLocation();
+    public BookLocation getBookLocation();
+    public Movement getMovement();
 }
