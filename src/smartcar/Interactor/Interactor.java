f89c5b7af3ba29de24c70c4d6e8979ad53f4690d
JackYang <jackyang7474@gmail.com>
2014-04-18 13:18:01 +0800

    完成InteractorIf文件

--- src/smartcar/Interactor/Interactor.java
+++ src/smartcar/Interactor/Interactor.java
@@ -1,25 +1,57 @@
 package smartcar.Interactor;
 
+import smartcar.Controller.Controller;
 import smartcar.core.Point;
 import smartcar.map.SmartMap;
 
+public class Interactor implements InteractorIf {
 
-public class Interactor {
     private SmartMap map;
-    private Point cLocation;
-    private Point bLocation;
-    public Interactor(SmartMap map){
-        this.map = map;
+    private Controller controller;
+
+    public Interactor() {
+        map = new SmartMap();
+        controller = new Controller(map);
+
     }
-    //set the operation for the car
-    
-    //set the location of the car
-    public void setCarLocation(double x,double y){
-    //TODO
-        
-    }
-    public SmartMap getMap(){
+
+    public SmartMap getSmartMap() {
         return map;
     }
-    
+
+    @Override
+    public void setOperation(int op) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setCarAutoDriveDestination(Point location) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public SmartMap getSmartMap() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Point getCarCurrentLocation() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setCar(int speed, int angle) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setCarClockwise() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setCarCounterClockwise() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
