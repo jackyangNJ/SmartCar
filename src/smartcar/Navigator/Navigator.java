4a9f9aab18b51f8eccf8d732e44b7ae8bc027df8
JackYang <jackyang7474@gmail.com>
2014-05-03 20:55:13 +0800

    fix bug

--- src/smartcar/Navigator/Navigator.java
+++ src/smartcar/Navigator/Navigator.java
@@ -17,24 +17,24 @@ import smartcar.Sensor.SensorMagnetic;
 import smartcar.Sensor.SensorMagneticData;
 import smartcar.Sensor.SensorMagneticIf;
 import smartcar.core.SystemCoreData;
+import smartcar.map.SmartMapInterface;
 
 /**
  *
  * @author jack
  */
-public class Navigator implements NavigatorIf{
-    
-    private static final double possibility_hall = 1/2;
-    private static final double possibility_acc = 1/2;
-    private static final double possibility_gory = 1/2;
+public class Navigator implements NavigatorIf {
+
+    private static final double possibility_hall = (double) 1 / 2;
+    private static final double possibility_acc = (double) 1 / 2;
+    private static final double possibility_gory = (double) 1 / 2;
     double frequency;
-    SmartMap map;
+    SmartMapInterface map;
     //传感器对象
     SensorHallIf sensorHall = new SensorHall();
     SensorAccIf sensorAcc = new SensorAcc();
     SensorGyroIf sensorGyro = new SensorGyro();
     SensorMagneticIf sensorMagnetic = new SensorMagnetic();
-    
 
     //传感器数据
     SensorHallData sensorHallData;
@@ -45,29 +45,10 @@ public class Navigator implements NavigatorIf{
     SensorMagneticData sensorMagneticData;
     NavigatorData nevigatorData;
     ArrayList list = new ArrayList(10);
-    
-    
-    
-    
+
     /**
      * 霍尔传感器事件处理函数
      */
-    //
-    /*
-    float dealangular(float angular){
-          if(angular > 2 * Math.PI * 3 / 4){
-                angular = (float) (2 * Math.PI - angular);
-            }
-            else if(angular > Math.PI ){
-                 angular = (float) (2 * Math.PI * 3 / 4 - angular);
-            }
-            else if(angular > 1 / 2 * Math.PI){
-                angular = (float)( Math.PI) - angular;
-            }
-          return angular;
-    }*/
-    
-    
     SensorListener sensorHallListener = new SensorListener() {
 
         @Override
@@ -76,59 +57,33 @@ public class Navigator implements NavigatorIf{
             /**
              * 目前这段代码假定的是小车在1次测距离时转动的角度不到180度
              */
-            
-            /*float distance = sensorHallData.getDriveDistance() - nevigatorData.getdistance();
-            float nowangular = sensorGyroData.getangular();
-            float historyangular = nevigatorData.getangular();
-            float angular = 0;//yong
-            float angulardifferience = nowangular - historyangular;
-            float linedistance = (float) (2 * Math.sin(Math.abs(angulardifferience) / 2)* distance /(Math.abs(angulardifferience)));//计算切线距离
-            */
-            
             // s not ensure
-            sensorHallData = (SensorHallData)e.getData();
+            sensorHallData = (SensorHallData) e.getData();
             double s = sensorHallData.getDriveDistance();
-        
-            double sum = 0;  
-             // 遍历求和  
-            for (int i = 0, size = list.size(); i < size; i++) {  
-                 sum += (double)list.get(i);  
-            }  
-            double averageangular = sum / list.size();  
-            double x = nevigatorData.getx() + (double)(s * Math.cos((double) averageangular));
+
+            double sum = 0;
+            // 遍历求和  
+            for (int i = 0, size = list.size(); i < size; i++) {
+                sum += (double) list.get(i);
+            }
+            double averageangular = sum / list.size();
+            double x = nevigatorData.getx() + (double) (s * Math.cos((double) averageangular));
             nevigatorData.setx(x);
-            double y = nevigatorData.gety() + (double)(s * Math.sin((double) averageangular));
+            double y = nevigatorData.gety() + (double) (s * Math.sin((double) averageangular));
             nevigatorData.sety(y);
             // t not ensure
-            if(SystemCoreData.getSystemState() != SystemCoreData.STATE_STILL){
-                double vx = possibility_hall * nevigatorData.getv_x() + (1 - possibility_hall) * (double)(s * Math.cos((double) averageangular))* frequency;
+            if (SystemCoreData.getSystemState() != SystemCoreData.STATE_STILL) {
+                double vx = possibility_hall * nevigatorData.getv_x() + (1 - possibility_hall) * (double) (s * Math.cos((double) averageangular)) * frequency;
                 nevigatorData.setv_x(vx);
-                double vy = possibility_hall * nevigatorData.getv_y() + (1 - possibility_hall) * (double)(s * Math.sin((double) averageangular))* frequency;
+                double vy = possibility_hall * nevigatorData.getv_y() + (1 - possibility_hall) * (double) (s * Math.sin((double) averageangular)) * frequency;
                 nevigatorData.setv_y(vy);
-            }
-            else{
+            } else {
                 nevigatorData.setv_x(0);
                 nevigatorData.setv_y(0);
             }
-             
-            
-            
-            
-          
-          
+
             nevigatorData.setdistance(sensorHallData.getDriveDistance());
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-          
+
         }
     };
     /**
@@ -140,9 +95,9 @@ public class Navigator implements NavigatorIf{
         public void SensorEventProcess(SensorEvent e) {
             //!TODO
             //a little diffirence from instruction
-            sensorAccRawData = (SensorAccData)e.getData();
-           
-            if(SystemCoreData.getSystemState() != SystemCoreData.STATE_STILL){
+            sensorAccRawData = (SensorAccData) e.getData();
+
+            if (SystemCoreData.getSystemState() != SystemCoreData.STATE_STILL) {
                 double vx = possibility_acc * nevigatorData.getv_x() + (1 - possibility_acc) * sensorAccRawData.getv_x();
                 nevigatorData.setv_x(vx);
                 sensorAccData.setv_x(vx);
@@ -157,25 +112,24 @@ public class Navigator implements NavigatorIf{
                 sensorAccData.setv_x(vx);
                 sensorAccData.setx(nevigatorData.getx());
                 sensorAccData.sety(nevigatorData.gety());
-                
-                
-            }
-            else{
-                 nevigatorData.setv_x(0);
-                 nevigatorData.setv_y(0);
-                 nevigatorData.seta_x(0);
-                 nevigatorData.seta_y(0);
-                 sensorAccData.setv_x(0);
-                 sensorAccData.setv_y(0);
-                 sensorAccData.seta_x(0);
-                 sensorAccData.seta_y(0);
-                 sensorAccData.setx(nevigatorData.getx());
-                 sensorAccData.sety(nevigatorData.gety());
-               
+
+            } else {
+                nevigatorData.setv_x(0);
+                nevigatorData.setv_y(0);
+                nevigatorData.seta_x(0);
+                nevigatorData.seta_y(0);
+                sensorAccData.setv_x(0);
+                sensorAccData.setv_y(0);
+                sensorAccData.seta_x(0);
+                sensorAccData.seta_y(0);
+                sensorAccData.setx(nevigatorData.getx());
+                sensorAccData.sety(nevigatorData.gety());
+
             }
             e.setData(sensorAccData);
         }
     };
+
     /**
      * 陀螺仪传感器事件处理函数
      */
@@ -183,21 +137,19 @@ public class Navigator implements NavigatorIf{
 
         @Override
         public void SensorEventProcess(SensorEvent e) {
-             //To change body of generated methods, choose Tools | Templates.
-            sensorGyroRawData = (SensorGyroData)e.getData();
+            sensorGyroRawData = (SensorGyroData) e.getData();
             nevigatorData.setangular((float) sensorGyroRawData.getHori_angle());
-            if(list.size() >= 10){
+            if (list.size() >= 10) {
                 list.remove(0);
             }
-           
-            float curangulat = (float) (possibility_gory * nevigatorData.getangular() +  (1 - possibility_gory) * sensorGyroRawData.getHori_angle());
+
+            float curangulat = (float) (possibility_gory * nevigatorData.getangular() + (1 - possibility_gory) * sensorGyroRawData.getHori_angle());
             nevigatorData.setangular(curangulat);
             list.add(curangulat);
-            
-            nevigatorData.setangular_velocity(sensorGyroRawData. getHori_angleSpeed());
+
+            nevigatorData.setangular_velocity(sensorGyroRawData.getHori_angleSpeed());
             sensorGyroData.setHori_angle(curangulat);
-            sensorGyroData.setHori_angleSpeed(sensorGyroRawData. getHori_angleSpeed());
-            e.setData(sensorGyroData);
+            sensorGyroData.setHori_angleSpeed(sensorGyroRawData.getHori_angleSpeed());
         }
     };
     /**
@@ -208,10 +160,9 @@ public class Navigator implements NavigatorIf{
         @Override
         public void SensorEventProcess(SensorEvent e) {
             //To change body of generated methods, choose Tools | Templates.
-           //  nevigatorData.setangular(sensorMagneticData.getangular());
+            //  nevigatorData.setangular(sensorMagneticData.getangular());
         }
     };
-
 
     /**
      *
@@ -219,48 +170,36 @@ public class Navigator implements NavigatorIf{
      */
     public Navigator(SmartMap map) {
         this.map = map;
-        
+        nevigatorData = new NavigatorData();
         //注册监听器
         sensorHall.addSenserListener(sensorHallListener);
         sensorGyro.addSenserListener(sensorGyroListener);
         sensorAcc.addSenserListener(sensorAccListener);
         sensorMagnetic.addSenserListener(sensorMagneticListener);
-   //     qrCode.addSenserListener(QRCodeListener);
     }
 
-    public Navigator() {
-       nevigatorData = new NavigatorData();
-    }
-    
-    
+
     public NavigatorData getNavigatorData() {
-       return nevigatorData;
+        return nevigatorData;
     }
 
-   
     public SensorAccData getSensorAccRawData() {
-       return sensorAcc.getSensorRawData();
+        return sensorAcc.getSensorRawData();
     }
-
-  
 
     @Override
     public SensorGyroData getSensorGyroRawData() {
-       return sensorGyro.getRawSensorGyroData();
+        return sensorGyro.getRawSensorGyroData();
     }
-
-  
 
     @Override
     public SensorMagneticData getSensorMagneticRawData() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-   
-
     @Override
     public SensorHallData getSensorHallData() {
-          return sensorHallData;
+        return sensorHallData;
     }
 
     @Override
